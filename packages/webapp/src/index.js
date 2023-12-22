cdb212148a074dfe06ba3df5fdfbbed1a03e1701
SriHarsha-Godavarthi <sriharsha.godavarthi232@gmail.com>
2023-12-22 14:13:24 -0500

    initial react app

---
+++ packages/webapp/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import  { createRoot }  from 'react-dom/client';
+// import App from './src/App.js'
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<h1>react app</h1>);
